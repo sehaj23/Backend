2d0154cf9232ace1dcf85fce49a3586468cf024d
Preet <preetsc27@gmail.com>
2020-03-17 02:33:05 -0500

    login implemented

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,14 @@ import * as path from "path"
 import { sequelize } from "./database"
 import Photo, { PhotoI } from "./models/photo.model"
 import Event, { EventI } from "./models/event.model"
+import loginRouter from "./controller/login.controller"
 const app = express()
 
 const accessLogStream = fs.createWriteStream('access.log', { flags: 'a' })
- 
+
 // setup the logger
 app.use(morgan('combined', { stream: accessLogStream }))
- 
+
 app.use(bobyParser.json())
 sequelize.authenticate().then(() => {
     console.log("Database connected...")
@@ -21,28 +22,34 @@ sequelize.authenticate().then(() => {
     console.log(`Error: ${err.message}`)
 })
 
+app.use("/api/login", loginRouter)
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
+});
 app.get("/p", async (req: express.Request, res: express.Response) => {
-  
-    const eId= 1
+
+    const eId = 1
     const e = await Event.findByPk(eId)
-    if(e!= null)
-    if(e?.photo_ids !== undefined){
-        const p = await Photo.findAll({where: {id: e.photo_ids}})
-        res.send({
-            e,
-            p
-        })
-        return
-    }
-    
-    if(e != null){
-        
+    if (e != null)
+        if (e?.photo_ids !== undefined) {
+            const p = await Photo.findAll({ where: { id: e.photo_ids } })
+            res.send({
+                e,
+                p
+            })
+            return
+        }
+
+    if (e != null) {
+
         // const photo: PhotoI ={
         //     name: "My Pic",
         //     url: "http",
         //     photo_type: 'Original'
         // }
-    
+
         // const p = await Photo.create(photo)
         // console.log(p)
         // if(e.photo_ids === undefined || e.photo_ids == null){
@@ -57,14 +64,14 @@ app.get("/p", async (req: express.Request, res: express.Response) => {
             e,
             // p
         })
-    }else{
+    } else {
         res.send(e)
     }
 })
 
-app.get("/", async (req: express.Request, res: express.Response) => {
-    
-    const event : EventI = {
+app.get("/aaapp", async (req: express.Request, res: express.Response) => {
+
+    const event: EventI = {
         name: "Cool Event",
         start_date: new Date('02-27-2019 10:00'),
         end_date: new Date('02-27-2019 12:00'),
@@ -73,22 +80,22 @@ app.get("/", async (req: express.Request, res: express.Response) => {
         exhibition_house: "Chhabra House of Design",
         description: "It is a cool event",
     }
-    
+
     try {
         const e = await Event.create(event)
         console.log(e);
-        const photo: PhotoI ={
+        const photo: PhotoI = {
             name: "My Pic",
             url: "http",
             photo_type: 'Original'
         }
-    
+
         const p = await Photo.create(photo)
         console.log(p)
-        if(e.photo_ids === undefined || e.photo_ids == null){
+        if (e.photo_ids === undefined || e.photo_ids == null) {
             const nwA = [p.id!]
             e.photo_ids = nwA
-        }else
+        } else
             e.photo_ids?.push(p.id!)
         const newE = await e.save()
         console.log(newE);
@@ -99,18 +106,18 @@ app.get("/", async (req: express.Request, res: express.Response) => {
         })
     } catch (error) {
         console.log(error);
-        
+
     }
-    
+
 })
 
 const PORT = 8080
 
 app.listen(PORT, async () => {
     try {
-        await sequelize.sync({force: true})
+        // await sequelize.sync({ force: true })
     } catch (error) {
-        
+
     }
     console.log(`Server is running http://localhost:${PORT}`)
 })
