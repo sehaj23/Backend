6cbf318b7f5a3f36519d5f095d9eb0eed38dd847
Preet <preetsc27@gmail.com>
2020-03-19 02:48:12 -0500

    add event and edit admin done updates

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4268b54d64d498ea273327185e1c2f56.js"
+  "/precache-manifest.358a265d0a8fa6ab43557088276ff79a.js"
 );
 
 self.addEventListener('message', (event) => {
