52603c5ad529ad848c01c7a1d60887126aeff103
Preet <preetsc27@gmail.com>
2020-03-19 02:58:59 -0500

    bug fixese

---
+++ build/precache-manifest.358a265d0a8fa6ab43557088276ff79a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc56b062eadda55c8801db7a6185cde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7dec1cc98483c2527197",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eafe4b970cf9eff22ad5",
+    "url": "/static/js/2.4e30c894.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.4e30c894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dec1cc98483c2527197",
+    "url": "/static/js/main.6063d5fe.chunk.js"
+  },
+  {
+    "revision": "c4eb3d7e1ee7e24f68cd",
+    "url": "/static/js/runtime-main.c7bbf249.js"
+  }
+]);
