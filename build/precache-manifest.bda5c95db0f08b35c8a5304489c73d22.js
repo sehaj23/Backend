eeb297554301e8597e7fd5ddb503f127429ea2c6
Preet <preetsc27@gmail.com>
2020-03-24 04:50:46 -0500

    new build 2

---
+++ build/precache-manifest.bda5c95db0f08b35c8a5304489c73d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925fe8792f9a6716e36fe398f1e6b482",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd20092ec019d8c378a9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3abdadb76c445f9a7689",
+    "url": "/static/js/2.dd1c3ee9.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.dd1c3ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd20092ec019d8c378a9",
+    "url": "/static/js/main.a9e5c181.chunk.js"
+  },
+  {
+    "revision": "c4eb3d7e1ee7e24f68cd",
+    "url": "/static/js/runtime-main.c7bbf249.js"
+  }
+]);
