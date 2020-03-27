745f08cce94191cef6a7f748c4c3dca259c95acd
Gammy <preetsc27@gmail.com>
2020-03-27 01:13:51 -0500

    add build folder

---
+++ precache-manifest.133d6cbe0f57e412e473149b7df526e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c43c138f13945c9a78ea3d7434de2fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04baa79a64c4d778bd08",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5e02cce1499c9cd122c5",
+    "url": "/static/js/2.5eb43018.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.5eb43018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04baa79a64c4d778bd08",
+    "url": "/static/js/main.458d0499.chunk.js"
+  },
+  {
+    "revision": "c4eb3d7e1ee7e24f68cd",
+    "url": "/static/js/runtime-main.c7bbf249.js"
+  }
+]);
