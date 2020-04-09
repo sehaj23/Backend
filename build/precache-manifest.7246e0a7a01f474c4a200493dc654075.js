de96597982f1167ebaa9e27dde0e3f89b6920be0
Gammy <preetsc27@gmail.com>
2020-04-09 11:57:17 -0500

    bug fix

---
+++ build/precache-manifest.7246e0a7a01f474c4a200493dc654075.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cf3ffc82d8d984a75c6fa46db50948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "116e513fc9d53dc6cfbf",
+    "url": "/static/css/main.5b52f5f7.chunk.css"
+  },
+  {
+    "revision": "dc01b1201487ce2b27d4",
+    "url": "/static/js/2.d726bc05.chunk.js"
+  },
+  {
+    "revision": "b0382a1d868abef989ec0625e2034248",
+    "url": "/static/js/2.d726bc05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "116e513fc9d53dc6cfbf",
+    "url": "/static/js/main.6bca33b5.chunk.js"
+  },
+  {
+    "revision": "68a6139eefb10aafa95d",
+    "url": "/static/js/runtime-main.21b3b292.js"
+  },
+  {
+    "revision": "dcab24a49c914127004f537d6b43504f",
+    "url": "/static/media/arrow.dcab24a4.svg"
+  },
+  {
+    "revision": "1c4b033c82b2ac494e50a1382ac5ae24",
+    "url": "/static/media/return.1c4b033c.svg"
+  }
+]);
---
+++ build/precache-manifest.774bd3fa2edb3bbcc0d4b137f11349eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af8d8cf87f34d0857cc11df77260672d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "848f97fbc443de2a1c25",
+    "url": "/static/css/main.5b52f5f7.chunk.css"
+  },
+  {
+    "revision": "dc01b1201487ce2b27d4",
+    "url": "/static/js/2.d726bc05.chunk.js"
+  },
+  {
+    "revision": "b0382a1d868abef989ec0625e2034248",
+    "url": "/static/js/2.d726bc05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "848f97fbc443de2a1c25",
+    "url": "/static/js/main.e88b0116.chunk.js"
+  },
+  {
+    "revision": "68a6139eefb10aafa95d",
+    "url": "/static/js/runtime-main.21b3b292.js"
+  },
+  {
+    "revision": "dcab24a49c914127004f537d6b43504f",
+    "url": "/static/media/arrow.dcab24a4.svg"
+  },
+  {
+    "revision": "1c4b033c82b2ac494e50a1382ac5ae24",
+    "url": "/static/media/return.1c4b033c.svg"
+  }
+]);
