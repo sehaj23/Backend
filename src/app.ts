345262d52079e39b5ac794b24471aefbc3695072
Gammy <preetsc27@gmail.com>
2020-04-14 06:30:22 -0500

    removed the frontend bug fix

--- src/app.ts
+++ src/app.ts
@@ -75,11 +75,7 @@ app.get(
     }
   }
 );
-app.use(express.static(path.join(__dirname, "../build")));
 
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "../build", "index.html"));
-});
 
 app.post(
   "/create-vendor",
