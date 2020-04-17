0f0fd880088bd54d903fea77d880f0176769ce67
Gammy <preetsc27@gmail.com>
2020-04-16 20:35:08 -0500

    ci test 3

--- src/database.ts
+++ src/database.ts
@@ -13,7 +13,7 @@ const password: string = process.env.DB_PASS ?? "postgres";
 
 export const connectt = () => {
   console.log("*************")
-    console.log(`process.env.DB_URI: ${process.env.DB_URI}`)
+    console.log(`process.env.DB_URI: ${uri}`)
     console.log("*************")
   if (process.env.NODE_ENV === "test") {
     
