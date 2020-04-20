9eea1e35282ad4cc84b09e54bdf467d97155c72b
Gammy <preetsc27@gmail.com>
2020-04-20 06:20:21 -0500

    booking routes add bug fix 1

---
+++ src/routes/booking.router.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import verifyToken from "../middleware/jwt";
+import VendorService from "../service/vendor.service";
+import BookinkService from "../service/booking.service";
+
+const bookingRouter = Router()
+const vs = new BookinkService()
+
+bookingRouter.post("/", verifyToken, vs.post)
+bookingRouter.get("/", verifyToken, vs.get)
+bookingRouter.get("/:id", verifyToken, vs.getId)
+bookingRouter.put("/:id", verifyToken, vs.put)
+
+export default bookingRouter
