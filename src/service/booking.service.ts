e9d741b2fa62d2c8e823c2693900d791d2e96f4e
Gammy <preetsc27@gmail.com>
2020-04-22 12:25:55 -0500

    booking add bug fix 5

--- src/service/booking.service.ts
+++ src/service/booking.service.ts
@@ -3,6 +3,7 @@ import Booking from "../models/booking.model"
 import { Request, Response } from "express"
 import { BookingI } from "../interfaces/booking.interface"
 import logger from "../utils/logger"
+import mongoose from "../database"
 
 
 export default class BookinkService extends BaseService{
@@ -12,17 +13,26 @@ export default class BookinkService extends BaseService{
     }
 
     post = async (req: Request, res: Response) => {
-        const d: BookingI = req.body
-        if(!d.salon_id && !d.makeup_artist_id && !d.designer_id){
-            const errMsg = `Atleast one provider is is reqired out of 3`
-            logger.error(errMsg)
-            res.status(400)
-            res.send({message: errMsg})
-            return
-        }
         try {
-           
             const e = req.body
+           
+            if(!e.salon_id && !e.makeup_artist_id && !e.designer_id){
+                const errMsg = `Atleast one provider is is reqired out of 3`
+                logger.error(errMsg)
+                res.status(400)
+                res.send({message: errMsg})
+                return
+            }
+
+            if(e.salon_id){
+                e.salon_id = mongoose.Types.ObjectId(e.salon_id)
+            }
+            if(e.makeup_artist_id){
+                e.makeup_artist_id = mongoose.Types.ObjectId(e.makeup_artist_id)
+            }
+            if(e.designer_id){
+                e.designer_id = mongoose.Types.ObjectId(e.designer_id)
+            }
 
             const event = await Booking.create(e)
 
