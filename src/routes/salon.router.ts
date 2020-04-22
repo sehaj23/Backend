614226b3a0ff715f6a4ef306252902dce6a09cc6
play.js <git@gituser.com>
2020-04-22 02:30:15 -0500

    Salon service route fix

--- src/routes/salon.router.ts
+++ src/routes/salon.router.ts
@@ -9,7 +9,7 @@ salonRouter.get("/:id", verifyToken, ss.getId)
 salonRouter.post("/", verifyToken, ss.post)
 salonRouter.put("/:id", verifyToken, ss.put)
 salonRouter.post("/event", verifyToken, ss.addSalonEvent)
-salonRouter.put("/salon", verifyToken, ss.addSalonService)
+salonRouter.put("/:id/salon", verifyToken, ss.addSalonService)
 salonRouter.put("/:id/photo", verifyToken, ss.putPhoto)
 salonRouter.get("/:id/photo", verifyToken, ss.getPhoto)
 
