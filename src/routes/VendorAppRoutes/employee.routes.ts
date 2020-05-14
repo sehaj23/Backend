0d8a05607c14d73c5ef6ea293d530ffd0cda5cf7
Gammy <preetsc27@gmail.com>
2020-05-14 01:57:33 -0500

    build failing bug fix

--- src/routes/VendorAppRoutes/employee.routes.ts
+++ src/routes/VendorAppRoutes/employee.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import Employeeverifytoken from "../../middleware/Employee.Jwt";
 import EmployeeService from "../../service/VendorAppService/employee.service";
 const employeeRouter = Router()
 
