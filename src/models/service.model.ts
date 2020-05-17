aa55b26576eb6daa725b91719203219b2a924d52
Gammy <preetsc27@gmail.com>
2020-05-17 06:26:35 -0500

    gender remove

--- src/models/service.model.ts
+++ src/models/service.model.ts
@@ -12,12 +12,6 @@ const ServiceSchema = new mongoose.Schema({
         required: true,
         min: 0
     },
-    gender:{
-        type:String,
-        required:true,
-        enum:["women","men"]
-
-    },
     duration:{
         type: Number,
         default: 15,
