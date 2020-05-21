19dbaea70b95d687ac5df710661259028e217a48
Sehaj Chawla <sehaj23rocks@gmail.com>
2020-05-21 22:50:53 +0530

    designer update

--- src/routes/VendorRoutes/designer.router.ts
+++ src/routes/VendorRoutes/designer.router.ts
@@ -8,6 +8,7 @@ const ds = new DesignerService()
 
 designerRouter.post("/",VendorverifyToken,ds.post)
 designerRouter.patch("/:id",VendorverifyToken,ds.patchDesigner)
+designerRouter.get("/:id",VendorverifyToken,ds.getId)
 designerRouter.put("/settings/:id",VendorverifyToken,ds.designerSettings)
 
 export default designerRouter
