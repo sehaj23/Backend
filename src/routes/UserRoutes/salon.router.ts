fc983a296a809e41e7c506b7ca43f5f2a5345581
Aman Thakur <45814337+aman56thakur@users.noreply.github.com>
2020-07-02 03:25:28 +0530

    pre-bhasad

--- src/routes/UserRoutes/salon.router.ts
+++ src/routes/UserRoutes/salon.router.ts
@@ -7,12 +7,11 @@ const salonInfoRouter = Router();
 salonInfoRouter.get("/info/:id", ss.getSalonInfo);
 // get names of  all salons
 salonInfoRouter.get("/names", ss.getSalonNames);
-
-//salonInfoRouter.get("/services", ss.getServiceNames);
+// get names of  services
+salonInfoRouter.get("/services", ss.getServiceNames);
 //get nearby salon range 2km
 salonInfoRouter.get("/location",ss.getSalonNearby)
 //sort by distance
 salonInfoRouter.get("/distance",ss.getSalonDistance)
-// search by service
-salonInfoRouter.get("/salon/services", ss.getSalonService);
+
 export default salonInfoRouter;
