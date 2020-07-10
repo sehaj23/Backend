5d4a62cd6fbc58830360ac19318caff786177dee
Preet Singh Chabra <sehaj23chawla@gmail.com>
2020-07-10 11:51:02 +0530

    date_time removed from booking model

--- src/models/booking.model.ts
+++ src/models/booking.model.ts
@@ -83,11 +83,6 @@ const BookingSchema = new mongoose.Schema({
         required: true,
         default: 0
     },
-    date_time: {
-        type: Date,
-        required: true,
-        default: Date.now()
-    },
     location: {
         type: String,
         enum: ['Customer Place' , 'Vendor Place'],
