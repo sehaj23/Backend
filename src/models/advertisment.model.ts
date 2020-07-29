242f3a85480c2c69ee4acd759a5245d658c4dde4
Preet Singh Chabra <sehaj23chawla@gmail.com>
2020-07-30 00:22:37 +0530

    advertisment model

--- src/models/advertisment.model.ts
+++ src/models/advertisment.model.ts
@@ -2,47 +2,46 @@ import mongoose from "../database";
 
 
 const AdvertismentSchema = new mongoose.Schema({
-    
+
     photo_ids: {
-        type: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "photos"
-        }]
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "photos",
+        required: true
     },
-    salon_id:{
+    salon_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "salon"
     },
-    name:{
+    name: {
         required: true,
-        type:String
+        type: String
     },
-    description:{
-        required:true,
-        type:String
+    description: {
+        required: true,
+        type: String
     },
-    price:{
-        required:true,
-        type:Number
+    price: {
+        required: true,
+        type: Number
     },
-    offer_price:{
-        required:true,
-        type:Number
+    offer_price: {
+        required: true,
+        type: Number
     },
     services: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "services"
     },
-    valid_from:{
+    valid_from: {
         type: Date,
-        required:true
+        required: true
     },
-    valid_to:{
+    valid_to: {
         type: Date,
-        required:true
+        required: true
     }
-    
-    
+
+
 
 
 })
