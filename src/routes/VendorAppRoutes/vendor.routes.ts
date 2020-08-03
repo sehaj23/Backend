5c3eb002b1f737c6ed6317e616b573c04e916a3a
Preet Singh Chabra <sehaj23chawla@gmail.com>
2020-08-03 12:35:14 +0530

    vendor router

--- src/routes/VendorAppRoutes/vendor.routes.ts
+++ src/routes/VendorAppRoutes/vendor.routes.ts
@@ -1,20 +1,25 @@
-// import { Router } from "express";
-// import VendorverifyToken from "../../middleware/VendorJwt"
-// import VendorService from "../../service/vendor.service";
-// import verifyToken from "../../middleware/jwt";
-// const vendorRouter = Router()
-// const vs = new VendorService()
+import { Router } from "express";
+import VendorverifyToken from "../../middleware/VendorJwt"
+import VendorService from "../../service/vendor.service";
+import verifyToken from "../../middleware/jwt";
+import Vendor from "../../models/vendor.model";
+import EmployeeAbsenteeism from "../../models/employeeAbsenteeism.model";
+import VendorController from "../../controller/vendor.controller";
+const vendorRouter = Router()
 
-// vendorRouter.post("/",vs.vendorLogin);
-// vendorRouter.post("/absent",VendorverifyToken,vs.employeeAbsent)
-// vendorRouter.post("/absent/update",VendorverifyToken,vs.employeeAbsent)
-// vendorRouter.get("/",VendorverifyToken ,vs.getVendor)
-// vendorRouter.put("/", VendorverifyToken, vs.update)
-// vendorRouter.patch("/update/",VendorverifyToken,vs.updatePass)
-// vendorRouter.get("/employee-slots/:id",VendorverifyToken, vs.employeeSlots)
-// vendorRouter.get("/slots/:id",VendorverifyToken,vs.slots)
-// vendorRouter.get("/employee/:id",VendorverifyToken,vs.employeebyId)
+const vs = new VendorService(Vendor,EmployeeAbsenteeism)
+const vendorController = new VendorController(vs)
 
-// //vendorRouter.put("/profile-pic", VendorverifyToken, vs.putProfilePic)
+vendorRouter.post("/",vendorController.vendorLogin);
+vendorRouter.post("/absent",VendorverifyToken,vendorController.employeeAbsent)
+vendorRouter.post("/absent/update",VendorverifyToken,vendorController.employeeAbsent)
+vendorRouter.get("/",VendorverifyToken ,vendorController.vendorInfo)
+vendorRouter.put("/", VendorverifyToken, vendorController.update)
+vendorRouter.patch("/update/",VendorverifyToken,vendorController.updatePass)
+vendorRouter.get("/employee-slots/:id",VendorverifyToken, vendorController.employeeSlots)
+vendorRouter.get("/slots/:id",VendorverifyToken,vendorController.slots)
+vendorRouter.get("/employee/:id",VendorverifyToken,vendorController.employeebyId)
 
-// export default vendorRouter
+vendorRouter.put("/profile-pic", VendorverifyToken, vendorController.putProfilePic)
+
+export default vendorRouter
