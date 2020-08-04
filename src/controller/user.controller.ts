030063728efcb1526d30e895ea2d9b0e60445a47
Preet Singh Chabra <sehaj23chawla@gmail.com>
2020-08-05 00:01:10 +0530

    email update

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -36,7 +36,7 @@ export default class UserController extends BaseController{
      
         const updates = Object.keys(req.body)
         const d = req.body
-        const allowedupates = ["name", "age","gender","color_complextion","address","phone"]
+        const allowedupates = ["email","name", "age","gender","color_complextion","address","phone"]
         const isvalid = updates.every((update) => allowedupates.includes(update))
         console.log(isvalid)
         if (!isvalid) {
