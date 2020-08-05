75f064ef990cd26518ec078a950f7ee0c71ef45e
Preet Singh Chabra <sehaj23chawla@gmail.com>
2020-08-05 21:35:39 +0530

    error in build

--- src/routes/VendorRoutes/booking.router.ts
+++ src/routes/VendorRoutes/booking.router.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // import { Router } from "express";
 // import VendorverifyToken from "../../middleware/VendorJwt";
 // import BookinkService from "../../service/booking.service";
@@ -6,15 +5,14 @@
 // import Booking from "../../models/booking.model";
 // import Salon from "../../models/salon.model";
 // import SalonService from "../../service/salon.service";
-// import EmployeeAbsenteeism from "../../models/employeeAbsenteeism.model";
-// import EmployeeAbsentismService from "../../service/employee-absentism.service";
 // import Employee from "../../models/employees.model";
 // import Vendor from "../../models/vendor.model";
-// import Event from "../../models/event.model"
 // import Offer from "../../models/offer.model";
-// import Review from '../../models/review.model'
+// import EmployeeAbsentismService from "../../service/employee-absentism.service";
+// import EmployeeAbsenteeism from "../../models/employeeAbsenteeism.model";
+// import Event from "../../models/event.model";
+// import Review from "../../models/review.model";
 // import Brand from "../../models/brands.model";
-
 
 
 // const bookingRouter = Router()
@@ -22,47 +20,23 @@
 // const employeeAbsenteeism = new EmployeeAbsentismService(EmployeeAbsenteeism)
 // const salonService = new SalonService(Salon,Employee,Vendor,Event,Offer,Review,Booking,Brand)
 // const bookingController = new BookingController(bookingService,salonService,employeeAbsenteeism)
-=======
-import { Router } from "express";
-import VendorverifyToken from "../../middleware/VendorJwt";
-import BookinkService from "../../service/booking.service";
-import BookingController from "../../controller/booking.controller";
-import Booking from "../../models/booking.model";
-import Salon from "../../models/salon.model";
-import SalonService from "../../service/salon.service";
-import Employee from "../../models/employees.model";
-import Vendor from "../../models/vendor.model";
-import Offer from "../../models/offer.model";
-import EmployeeAbsentismService from "../../service/employee-absentism.service";
-import EmployeeAbsenteeism from "../../models/employeeAbsenteeism.model";
-import Event from "../../models/event.model";
-import Review from "../../models/review.model";
-import Brand from "../../models/brands.model";
 
 
-const bookingRouter = Router()
-const bookingService = new BookinkService(Booking,Salon)
-const employeeAbsenteeism = new EmployeeAbsentismService(EmployeeAbsenteeism)
-const salonService = new SalonService(Salon,Employee,Vendor,Event,Offer,Review,Booking,Brand)
-const bookingController = new BookingController(bookingService,salonService,employeeAbsenteeism)
->>>>>>> 9f05a8e4c29fdd7622e7f3d59396b77f65035a40
+// // bookingRouter.post("/", VendorverifyToken, bookingController.post)
+// // bookingRouter.get("/:id", VendorverifyToken, bookingController.getId)
+// // bookingRouter.get("/", VendorverifyToken, bookingController.getbookings) //filter can be used here
+// // bookingRouter.put("/assignEmployee/:id", VendorverifyToken, bookingController.assigneEmployeeBookings)
+// // bookingRouter.get("/all/salon/:id",VendorverifyToken,bookingController.getAllSalonBookings)  //test cases left
+// // bookingRouter.get("/all/makeupArtist/:id",VendorverifyToken,bookingController.getAllMuaBookings) //test cases left
+// // bookingRouter.get("/all/designer/:id",VendorverifyToken,bookingController.getAllDesignerBookings) //test cases left
+// // bookingRouter.get("/salon/:id",VendorverifyToken,bookingController.getSalonBookings) // bookings except pending
+// // bookingRouter.get("/makeupArtist/:id",VendorverifyToken,bookingController.getmakeupArtistBookings) //booking except pending
+// // bookingRouter.get("/designer/:id",VendorverifyToken,bookingController.getDesignerBookings) // booking except pending
+// // bookingRouter.get("/pending/salon/:id",VendorverifyToken,bookingController.getPendingSalonBookings)
+// // bookingRouter.get("/pending/makeupArtist/:id",VendorverifyToken,bookingController.getPendingmakeupArtistBookings)
+// // bookingRouter.get("/pending/designer/:id",VendorverifyToken,bookingController.getPendingDesignerBookings)
+// // bookingRouter.patch("/updatestatus/:id",VendorverifyToken,bookingController.updateStatusBookings)
+// // bookingRouter.patch("/reschedule/:id",VendorverifyToken,bookingController.reschedulebooking)
 
 
-// bookingRouter.post("/", VendorverifyToken, bookingController.post)
-// bookingRouter.get("/:id", VendorverifyToken, bookingController.getId)
-// bookingRouter.get("/", VendorverifyToken, bookingController.getbookings) //filter can be used here
-// bookingRouter.put("/assignEmployee/:id", VendorverifyToken, bookingController.assigneEmployeeBookings)
-// bookingRouter.get("/all/salon/:id",VendorverifyToken,bookingController.getAllSalonBookings)  //test cases left
-// bookingRouter.get("/all/makeupArtist/:id",VendorverifyToken,bookingController.getAllMuaBookings) //test cases left
-// bookingRouter.get("/all/designer/:id",VendorverifyToken,bookingController.getAllDesignerBookings) //test cases left
-// bookingRouter.get("/salon/:id",VendorverifyToken,bookingController.getSalonBookings) // bookings except pending
-// bookingRouter.get("/makeupArtist/:id",VendorverifyToken,bookingController.getmakeupArtistBookings) //booking except pending
-// bookingRouter.get("/designer/:id",VendorverifyToken,bookingController.getDesignerBookings) // booking except pending
-// bookingRouter.get("/pending/salon/:id",VendorverifyToken,bookingController.getPendingSalonBookings)
-// bookingRouter.get("/pending/makeupArtist/:id",VendorverifyToken,bookingController.getPendingmakeupArtistBookings)
-// bookingRouter.get("/pending/designer/:id",VendorverifyToken,bookingController.getPendingDesignerBookings)
-// bookingRouter.patch("/updatestatus/:id",VendorverifyToken,bookingController.updateStatusBookings)
-// bookingRouter.patch("/reschedule/:id",VendorverifyToken,bookingController.reschedulebooking)
-
-
-// export default bookingRouter
+// // export default bookingRouter
