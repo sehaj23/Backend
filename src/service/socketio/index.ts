452a40a5df304b3a341524fd7413e9cddafb1662
codersingh27 <codersingh27@gmail.com>
2020-09-14 00:50:37 -0500

    socket testing

--- src/service/socketio/index.ts
+++ src/service/socketio/index.ts
@@ -17,52 +17,52 @@ export const getVendorRoom = (vendorId: string) =>
     `${SocketRoomType.Admin} ${vendorId}`;
 
 const startSocketIO = (io: SocketIO.Server) => {
-    io.use(async (s:Socket, next) => {
-        logger.info("Socket incoming connection - Middle wear");
+    // io.use(async (s:Socket, next) => {
+    //     logger.info("Socket incoming connection - Middle wear");
 
-        const token: string = s.handshake.query.token;
-        if (!token) {
-            logger.error("Client Tried to connect without token");
-            s.disconnect();
-            return next(new Error("Client Tried to connect without token"));
-        }
+    //     const token: string = s.handshake.query.token;
+    //     if (!token) {
+    //         logger.error("Client Tried to connect without token");
+    //         s.disconnect();
+    //         return next(new Error("Client Tried to connect without token"));
+    //     }
 
-        // checking if it is admin or vendor
-        let socketConnectionType: SocketConnectionType;
-        const adminDecoded = await adminJWTVerification(token);
-        if (adminDecoded !== null)
-            socketConnectionType = SocketConnectionType.Admin;
-        const vendorDecoded = await vendorJWTVerification(token);
-        //@ts-ignore
-        if (vendorDecoded !== null && vendorDecoded._id)
-            socketConnectionType = SocketConnectionType.Vendor;
+    //     // checking if it is admin or vendor
+    //     let socketConnectionType: SocketConnectionType;
+    //     const adminDecoded = await adminJWTVerification(token);
+    //     if (adminDecoded !== null)
+    //         socketConnectionType = SocketConnectionType.Admin;
+    //     const vendorDecoded = await vendorJWTVerification(token);
+    //     //@ts-ignore
+    //     if (vendorDecoded !== null && vendorDecoded._id)
+    //         socketConnectionType = SocketConnectionType.Vendor;
 
-        if (!socketConnectionType) {
-            logger.error("Token not authenticated");
-            s.disconnect();
-            return next(new Error("Token not authenticated"));
-        }
-        s.request.socketConnectionType = socketConnectionType
-        return next();
+    //     if (!socketConnectionType) {
+    //         logger.error("Token not authenticated");
+    //         s.disconnect();
+    //         return next(new Error("Token not authenticated"));
+    //     }
+    //     s.request.socketConnectionType = socketConnectionType
+    //     return next();
 
-    });
+    // });
     io.sockets.on("connection", async (s: Socket) => {
         logger.info("Socket incoming connection");
-
+        s.emit("hello", "YOLO")
         // getting the token
-        const socketConnectionType = s.request.socketConnectionType
-        console.log(`socketConnectionType: ${socketConnectionType}`)
-        if (socketConnectionType === SocketConnectionType.Admin) {
-            s.on("subscribe-bookings", (data: string) => {
-                s.join(`${SocketRoomType.Admin}`);
-                s.emit("hello", "YOLO")
-            });
-        } else if (socketConnectionType === SocketConnectionType.Vendor) {
-            s.on("subscribe-bookings", (data: string) => {
-                //@ts-ignore
-                s.join(getVendorRoom("ABC"));
-            });
-        }
+        // const socketConnectionType = s.request.socketConnectionType
+        // console.log(`socketConnectionType: ${socketConnectionType}`)
+        // if (socketConnectionType === SocketConnectionType.Admin) {
+        //     s.on("subscribe-bookings", (data: string) => {
+        //         s.join(`${SocketRoomType.Admin}`);
+        //         s.emit("hello", "YOLO")
+        //     });
+        // } else if (socketConnectionType === SocketConnectionType.Vendor) {
+        //     s.on("subscribe-bookings", (data: string) => {
+        //         //@ts-ignore
+        //         s.join(getVendorRoom("ABC"));
+        //     });
+        // }
     });
 };
 
