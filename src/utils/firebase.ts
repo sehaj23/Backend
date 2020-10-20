1c1780a92b2fd227dbe8588f990ceda6b3c59018
codersingh27 <codersingh27@gmail.com>
2020-10-20 09:40:05 -0500

    server fixed firebase file update

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -9,7 +9,7 @@ import * as admin from 'firebase-admin';
 
 //var admin = require("firebase-admin");
 
-var serviceAccount = require("../../zattire-user-app-firebase-adminsdk-ud6n3-ac1bd907d8.json");
+var serviceAccount = require("../../zattire-vendor-app-firebase-adminsdk-c56y9-2cef91fd53.json");
 
 const firebase=admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
