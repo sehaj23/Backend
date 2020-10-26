6b929f9156b65f16a60c863c928fa4ef8fe9478a
codersingh27 <codersingh27@gmail.com>
2020-10-26 09:13:17 -0500

    null check for error message

--- src/middleware/controller-error-handler.middleware.ts
+++ src/middleware/controller-error-handler.middleware.ts
@@ -5,7 +5,7 @@ import logger from "../utils/logger";
 const controllerErrorHandler = fn => (req: Request, res: Response) => Promise.resolve(fn(req, res)).catch((e: ErrorResponse) => {
     logger.error(`${e.statusCode || 400} - ${e.message}`)
     logger.error(e.stack)
-    res.status(e.statusCode || 400).send({message: e.message})
+    res.status(e.statusCode || 400).send({message: e?.message ?? "Some Error"})
 })
 
 export default controllerErrorHandler
