f26401d2c025daa554bd7b41ef3639cfdf36959d
codersingh27 <codersingh27@gmail.com>
2020-11-09 14:08:23 -0600

    add ts-ignore to update fcm tokens

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,35 +1,24 @@
-import { Router, Request, Response } from "express";
-import BaseService from "./base.service";
-import User from "../models/user.model";
-import { PhotoI } from "../interfaces/photo.interface";
-import Photo from "../models/photo.model";
-import logger from "../utils/logger";
-import CONFIG from "../config";
-import UserI, { UserSI } from "../interfaces/user.interface";
-import * as crypto from "crypto"
 import mongoose from "../database";
+import { UserSI } from "../interfaces/user.interface";
 import encryptData from "../utils/password-hash";
-import { MigrationHubConfig } from "aws-sdk";
-import { Mongoose } from "mongoose";
-import ErrorResponse from "../utils/error-response";
-import { userJWTVerification } from "../middleware/User.jwt";
 import sendNotificationToDevice from "../utils/send-notification";
+import BaseService from "./base.service";
 
 export default class UserService extends BaseService {
     bookingModel: mongoose.Model<any, any>
-    constructor(User: mongoose.Model<any, any>,bookingModel:mongoose.Model<any,any>) {
+    constructor(User: mongoose.Model<any, any>, bookingModel: mongoose.Model<any, any>) {
         super(User)
-        this.bookingModel=bookingModel
+        this.bookingModel = bookingModel
     }
 
     getUser = async (userId) => {
         //@ts-ignore
-        const user = await this.model.findOne({_id:userId})
+        const user = await this.model.findOne({ _id: userId })
         console.log(user)
-        user.password=""
+        user.password = ""
         return user
     }
-    update = async (id: string,d: any) => {
+    update = async (id: string, d: any) => {
         const _id = mongoose.Types.ObjectId(id)
         const user = await this.model.findByIdAndUpdate(_id, d, { new: true })
         return user
@@ -43,41 +32,43 @@ export default class UserService extends BaseService {
         if (user !== null) {
             const updatepass = await this.model.findByIdAndUpdate({ _id }, { password: newpasswordHash }, { new: true })
             return updatepass
-        } 
+        }
         return null
     }
 
-    updateFCM = async (id:string,fcm_token:any) => {
+    updateFCM = async (id: string, fcm_token: any) => {
 
         const _id = mongoose.Types.ObjectId(id)
-        const user = await this.model.findByIdAndUpdate(_id, {$addToSet:{fcm_token:fcm_token}}, { new: true })
+        //@ts-ignore
+        const user = await this.model.findByIdAndUpdate(_id, { $addToSet: { fcm_token: fcm_token } }, { new: true })
         return user
     }
-    deleteFCM = async (id:string,fcm_token:any) => {
+    deleteFCM = async (id: string, fcm_token: any) => {
         const _id = mongoose.Types.ObjectId(id)
-        const user = await this.model.findByIdAndUpdate(_id, {$pull:{fcm_token:[fcm_token]}}, { new: true })
+        //@ts-ignore
+        const user = await this.model.findByIdAndUpdate(_id, { $pull: { fcm_token: [fcm_token] } }, { new: true })
         return user
     }
 
 
-    pastBooking = async(id:string)=>{
-        const booking = await this.bookingModel.find({user_id:id}).populate("employee_id")
+    pastBooking = async (id: string) => {
+        const booking = await this.bookingModel.find({ user_id: id }).populate("employee_id")
         return booking
     }
-    addAddress = async(id:string,d:any)=>{
-        const user = await this.model.findOne({_id:id}) as UserSI
+    addAddress = async (id: string, d: any) => {
+        const user = await this.model.findOne({ _id: id }) as UserSI
         user.address.push(d)
         await user.save()
         return user.address
     }
 
-    updateAddress = async(userId:string, addressId: string, d:Object)=>{
-        const user = await this.model.findOne({_id:userId, "address._id": mongoose.Types.ObjectId(addressId)}) as UserSI
-        if(user === null) throw new Error("User with this address id not found")
-        for(let add of user.address){
+    updateAddress = async (userId: string, addressId: string, d: Object) => {
+        const user = await this.model.findOne({ _id: userId, "address._id": mongoose.Types.ObjectId(addressId) }) as UserSI
+        if (user === null) throw new Error("User with this address id not found")
+        for (let add of user.address) {
             //@ts-ignore
-            if(add._id.toString() === addressId){
-                for(let key of  Object.keys(d)) add[key] = d[key]
+            if (add._id.toString() === addressId) {
+                for (let key of Object.keys(d)) add[key] = d[key]
                 await user.save()
                 return user.address
             }
@@ -85,13 +76,13 @@ export default class UserService extends BaseService {
         throw new Error("Address not found. Hence, not updated")
     }
 
-    deleteAddress = async(userId:string, addressId: string)=>{
-        const user = await this.model.findOne({_id:userId, "address._id": mongoose.Types.ObjectId(addressId)}) as UserSI
-        if(user === null) throw new Error("User with this address id not found. Delete")
-        for(let i = 0; i < user.address.length; i++){
+    deleteAddress = async (userId: string, addressId: string) => {
+        const user = await this.model.findOne({ _id: userId, "address._id": mongoose.Types.ObjectId(addressId) }) as UserSI
+        if (user === null) throw new Error("User with this address id not found. Delete")
+        for (let i = 0; i < user.address.length; i++) {
             const add = user.address[i]
             //@ts-ignore
-            if(add._id.toString() === addressId){
+            if (add._id.toString() === addressId) {
                 user.address.splice(i, 1)
                 await user.save()
                 return user.address
@@ -100,100 +91,103 @@ export default class UserService extends BaseService {
         throw new Error("Address not found. Hence, not deleted")
     }
 
-    getAddress = async(id:string)=>{
-        const address = await this.model.findById({_id:id}).select("address")
+    getAddress = async (id: string) => {
+        const address = await this.model.findById({ _id: id }).select("address")
         return address
     }
-    
 
-    addToFavourites = async (id:string,salon_id:string) => {
-       const salonid = mongoose.Types.ObjectId(salon_id)
-       console.log("salon id",salonid)
-       console.log("user",id)
-      // const user = await this.model.findOne({_id:id})
-    
-      //  const user = await this.model.findById({_id:id}) 
-      //@ts-ignore 
-          const user = await this.model.update({_id:id},{$push:{favourites:[salonid]}},{new:true})
 
-            return user
+    addToFavourites = async (id: string, salon_id: string) => {
+        const salonid = mongoose.Types.ObjectId(salon_id)
+        console.log("salon id", salonid)
+        console.log("user", id)
+        // const user = await this.model.findOne({_id:id})
+
+        //  const user = await this.model.findById({_id:id}) 
+        //@ts-ignore 
+        const user = await this.model.update({ _id: id }, { $push: { favourites: [salonid] } }, { new: true })
+
+        return user
 
     }
-    getFavourites = async (id:string,) => {
-        
-           const user = await this.model.findOne({_id:id}).select("favourites").populate({path:"favourites" ,select:{name:"name",rating:"rating",location:"location",profile_pic:"profile_pic"
-          },populate : {
-            path : 'profile_pic'
-          }})
- 
-            return user
- 
-     }
-     removeFavourites = async (id:string,salon_id:string) => {
+    getFavourites = async (id: string,) => {
+
+        const user = await this.model.findOne({ _id: id }).select("favourites").populate({
+            path: "favourites", select: {
+                name: "name", rating: "rating", location: "location", profile_pic: "profile_pic"
+            }, populate: {
+                path: 'profile_pic'
+            }
+        })
+
+        return user
+
+    }
+    removeFavourites = async (id: string, salon_id: string) => {
         console.log(salon_id)
         //@ts-ignore
-        const user = await this.model.findByIdAndUpdate({_id:id},{$pull:{favourites:salon_id}},{new:true})
+        const user = await this.model.findByIdAndUpdate({ _id: id }, { $pull: { favourites: salon_id } }, { new: true })
         console.log(user)
 
-         return user
+        return user
 
-  }
-  sendNotification = async (fcm_token:string,message:any)=>{
-    var messagee = {
-        notification: {
-          title: '$FooCorp up 1.43% on the day',
-          body: '$FooCorp gained 11.80 points to close at 835.67, up 1.43% on the day.'
-        },
-      };
-      
-      
-    const notification = sendNotificationToDevice(fcm_token,message)
-    return notification
-  }
+    }
+    sendNotification = async (fcm_token: string, message: any) => {
+        var messagee = {
+            notification: {
+                title: '$FooCorp up 1.43% on the day',
+                body: '$FooCorp gained 11.80 points to close at 835.67, up 1.43% on the day.'
+            },
+        };
 
 
-  searchUsersByEmail = async (q)=>{
-      
-    const pageNumber: number = parseInt(q.page_number || 1)
-    let pageLength: number = parseInt(q.page_length || 25)
-    pageLength = (pageLength > 100) ? 100 : pageLength
-    const skipCount = (pageNumber - 1) * pageLength
-    const keys = Object.keys(q)
-    const filters = {}
-    
-    for (const k of keys) {
-        switch (k) {
-            case "email":
-                filters["email"] = {
-                        $regex: `.*${q.email}.*`,$options:'i'
-                }
-                break
+        const notification = sendNotificationToDevice(fcm_token, message)
+        return notification
+    }
+
+
+    searchUsersByEmail = async (q) => {
+
+        const pageNumber: number = parseInt(q.page_number || 1)
+        let pageLength: number = parseInt(q.page_length || 25)
+        pageLength = (pageLength > 100) ? 100 : pageLength
+        const skipCount = (pageNumber - 1) * pageLength
+        const keys = Object.keys(q)
+        const filters = {}
+
+        for (const k of keys) {
+            switch (k) {
+                case "email":
+                    filters["email"] = {
+                        $regex: `.*${q.email}.*`, $options: 'i'
+                    }
+                    break
                 case "name":
                     filters["name"] = {
-                            $regex: `.*${q.name}.*`,$options:'i'
+                        $regex: `.*${q.name}.*`, $options: 'i'
                     }
-                break
+                    break
                 default:
                     filters[k] = q[k]
             }
         }
         console.log(filters)
-  const userDetailsReq =  this.model.find(filters).skip(skipCount).limit(pageLength).sort('-createdAt') 
-  const userPagesReq = this.model.count(filters)
-  //const userStatsReq = this.model.find(filters).skip(skipCount).limit(pageLength).sort('-createdAt')
+        const userDetailsReq = this.model.find(filters).skip(skipCount).limit(pageLength).sort('-createdAt')
+        const userPagesReq = this.model.count(filters)
+        //const userStatsReq = this.model.find(filters).skip(skipCount).limit(pageLength).sort('-createdAt')
 
 
-  const [userDetails,userPages] = await Promise.all([userDetailsReq, userPagesReq ])
-  return ({ userDetails,userPages })
+        const [userDetails, userPages] = await Promise.all([userDetailsReq, userPagesReq])
+        return ({ userDetails, userPages })
 
 
-}
-updateNewPass = async (id:string,email: string, password: string) => {
-    const passwordHash = encryptData(password)
-    return await this.model.findOneAndUpdate({_id:id,email:email},{password:passwordHash},{new:true})   
-   
-}
- 
+    }
+    updateNewPass = async (id: string, email: string, password: string) => {
+        const passwordHash = encryptData(password)
+        return await this.model.findOneAndUpdate({ _id: id, email: email }, { password: passwordHash }, { new: true })
+
+    }
+
 
 
 }
--- src/service/vendor.service.ts
+++ src/service/vendor.service.ts
@@ -1,216 +1,207 @@
-import logger from "../utils/logger";
-import * as crypto from "crypto"
-import Vendor from "../models/vendor.model";
-import { Router, Request, Response } from "express";
-import * as jwt from "jwt-then";
-import CONFIG from "../config";
-import { VendorI } from "../interfaces/vendor.interface";
-import { vendorJWTVerification } from "../middleware/VendorJwt"
-import mongoose from "../database";
-import { EmployeeAbsenteeismI } from "../interfaces/employeeAbsenteeism.interface"
-import EmployeeAbsenteeism from "../models/employeeAbsenteeism.model"
-import { Mongoose } from "mongoose";
-import BaseService from "./base.service";
-import { PhotoI } from "../interfaces/photo.interface";
-import Photo from "../models/photo.model";
-import Salon from "../models/salon.model";
-import Employee from "../models/employees.model"
-import moment = require("moment");
 import { String } from "aws-sdk/clients/appstream";
-import encryptData from "../utils/password-hash";
-import { ReportVendorI } from "../interfaces/reportVendor.interface";
+import mongoose from "../database";
+import { EmployeeAbsenteeismI } from "../interfaces/employeeAbsenteeism.interface";
 import { FeedbackI } from "../interfaces/feedback.interface";
+import { ReportVendorI } from "../interfaces/reportVendor.interface";
+import EmployeeAbsenteeism from "../models/employeeAbsenteeism.model";
+import Employee from "../models/employees.model";
+import Salon from "../models/salon.model";
+import encryptData from "../utils/password-hash";
+import BaseService from "./base.service";
+import moment = require("moment");
 
 
 
 
-export default class VendorService extends BaseService{
-    employeeAbsenteeismModel : mongoose.Model<any, any>
-    reportVendorModel : mongoose.Model<any, any>
-    feedbackVendorModel : mongoose.Model<any, any>
-    constructor(Vendor: mongoose.Model<any, any>, employeeAbsenteeismModel : mongoose.Model<any, any>,reportVendorModel : mongoose.Model<any, any>,feedbackVendorModel : mongoose.Model<any, any>) {
+export default class VendorService extends BaseService {
+    employeeAbsenteeismModel: mongoose.Model<any, any>
+    reportVendorModel: mongoose.Model<any, any>
+    feedbackVendorModel: mongoose.Model<any, any>
+    constructor(Vendor: mongoose.Model<any, any>, employeeAbsenteeismModel: mongoose.Model<any, any>, reportVendorModel: mongoose.Model<any, any>, feedbackVendorModel: mongoose.Model<any, any>) {
         super(Vendor)
-        this.employeeAbsenteeismModel=employeeAbsenteeismModel
-        this.reportVendorModel=reportVendorModel
-        this.feedbackVendorModel=feedbackVendorModel
+        this.employeeAbsenteeismModel = employeeAbsenteeismModel
+        this.reportVendorModel = reportVendorModel
+        this.feedbackVendorModel = feedbackVendorModel
     }
 
     // vendorLogin  = async (email,password) => {
-          
+
     //         const passwordHash = encryptData(password)
     //         const vendor = await this.model.findOne({ email, password: passwordHash })
     //         vendor.password=""
     //         const token = await jwt.sign(vendor.toJSON(), CONFIG.VENDOR_JWT, { expiresIn: "7 days" })
     //          return({ token })  //made change here for ID
-       
+
     // }
 
-    employeeAbsent = async (d:EmployeeAbsenteeismI) => {
+    employeeAbsent = async (d: EmployeeAbsenteeismI) => {
 
-            const absent = await (await this.employeeAbsenteeismModel.create(d)).populate("employee_id").execPopulate()
-            return absent
+        const absent = await (await this.employeeAbsenteeismModel.create(d)).populate("employee_id").execPopulate()
+        return absent
     }
 
     employeeAbsentUpdate = async (d: EmployeeAbsenteeismI) => {
 
-            //@ts-ignore
-            const check = await EmployeeAbsenteeism.findOneAndUpdate({ employee_id: d.employee_id, absenteeism_date: d.absenteeism_date }, d, { new: true }).populate("employee_id").exec()
-            return check
+        //@ts-ignore
+        const check = await EmployeeAbsenteeism.findOneAndUpdate({ employee_id: d.employee_id, absenteeism_date: d.absenteeism_date }, d, { new: true }).populate("employee_id").exec()
+        return check
 
     }
     getVendor = async (vendorId) => {
 
-            //@ts-ignore
-            
-            const vendor = await this.model.findOne({_id:vendorId}).populate("makeup_artists").populate("salons").populate("designers").populate("profile_pic").exec()
-           
-            return vendor
+        //@ts-ignore
+
+        const vendor = await this.model.findOne({ _id: vendorId }).populate("makeup_artists").populate("salons").populate("designers").populate("profile_pic").exec()
+
+        return vendor
 
     }
-    update = async (id:string,d:any) => {
+    update = async (id: string, d: any) => {
 
-            const _id = mongoose.Types.ObjectId(id)
-            const vendor = await this.model.findByIdAndUpdate(_id, d, { new: true })
-            return vendor
-    }
-    updateFCM = async (id:string,fcm_token:any) => {
         const _id = mongoose.Types.ObjectId(id)
-        const vendor = await this.model.findByIdAndUpdate(_id, {$addToSet:{fcm_token:fcm_token}}, { new: true })
+        const vendor = await this.model.findByIdAndUpdate(_id, d, { new: true })
         return vendor
     }
-    deleteFCM = async (id:string,fcm_token:any) => {
+    updateFCM = async (id: string, fcm_token: any) => {
         const _id = mongoose.Types.ObjectId(id)
-        const user = await this.model.findByIdAndUpdate(_id, {$pull:{fcm_token:[fcm_token]}}, { new: true })
+        //@ts-ignore
+        const vendor = await this.model.findByIdAndUpdate(_id, { $addToSet: { fcm_token: fcm_token } }, { new: true })
+        return vendor
+    }
+    deleteFCM = async (id: string, fcm_token: any) => {
+        const _id = mongoose.Types.ObjectId(id)
+        //@ts-ignore
+        const user = await this.model.findByIdAndUpdate(_id, { $pull: { fcm_token: [fcm_token] } }, { new: true })
         return user
     }
-        updatePass = async (id:string,password:string,newpassword:String) => {
-  
-            //@ts-ignore
-            const _id = mongoose.Types.ObjectId(id)
-            const passwordHash = encryptData(password)
-            const vendor = await this.model.findOne({ _id, password: passwordHash })
-            const newpasswordHash = encryptData(newpassword)
-            if (vendor) {
-                const updatepass = await this.model.findByIdAndUpdate({ _id }, { password: newpasswordHash }, { new: true })
-                return updatepass
-            } else {
-               return("Error Updating password")
-            }
+    updatePass = async (id: string, password: string, newpassword: String) => {
+
+        //@ts-ignore
+        const _id = mongoose.Types.ObjectId(id)
+        const passwordHash = encryptData(password)
+        const vendor = await this.model.findOne({ _id, password: passwordHash })
+        const newpasswordHash = encryptData(newpassword)
+        if (vendor) {
+            const updatepass = await this.model.findByIdAndUpdate({ _id }, { password: newpasswordHash }, { new: true })
+            return updatepass
+        } else {
+            return ("Error Updating password")
+        }
 
     }
-    employeeSlots = async (id:string,timeSlots:string) => {
-       
+    employeeSlots = async (id: string, timeSlots: string) => {
 
-           
-            //@ts-ignore
-            const empId = mongoose.Types.ObjectId(id)
-            console.log(empId)
-            
 
-            // getting the date from the frontend for which he needs the slots for
-            
 
-         
-          
-          
-            //@ts-ignore
-            const salonReq = Salon.findOne({ employees: [empId] })
-            //@ts-ignore
-            const employeesAbsenteeismReq = EmployeeAbsenteeism.findOne({ employee_id: empId, absenteeism_date: timeSlots })
-            const [salon, employeesAbsenteeism] = await Promise.all([salonReq, employeesAbsenteeismReq])
-            console.log(employeesAbsenteeism)
-            const starting_hours = salon.start_working_hours
-            var slots = starting_hours.map(function (val) {
-                const storeDate = moment(val).format('hh:mm a')
-                const employeeAbsentSlots = employeesAbsenteeism.absenteeism_times
-                if (employeeAbsentSlots.length === 0) {
-                    return {
-                        store_date: storeDate,
-                        absent: false
-                    }
-                }
-                for (let slot of employeeAbsentSlots) {
-                    slot = moment(slot).format('hh:mm a')
-                    if (slot === storeDate) {
-                        return {
-                            store_date: storeDate,
-                            absent: true
-                        }
-                    }
-                }
+        //@ts-ignore
+        const empId = mongoose.Types.ObjectId(id)
+        console.log(empId)
+
+
+        // getting the date from the frontend for which he needs the slots for
+
+
+
+
+
+        //@ts-ignore
+        const salonReq = Salon.findOne({ employees: [empId] })
+        //@ts-ignore
+        const employeesAbsenteeismReq = EmployeeAbsenteeism.findOne({ employee_id: empId, absenteeism_date: timeSlots })
+        const [salon, employeesAbsenteeism] = await Promise.all([salonReq, employeesAbsenteeismReq])
+        console.log(employeesAbsenteeism)
+        const starting_hours = salon.start_working_hours
+        var slots = starting_hours.map(function (val) {
+            const storeDate = moment(val).format('hh:mm a')
+            const employeeAbsentSlots = employeesAbsenteeism.absenteeism_times
+            if (employeeAbsentSlots.length === 0) {
                 return {
                     store_date: storeDate,
                     absent: false
                 }
-            })
-            return slots
-      
-    }
-    slots = async (salonid:string,reqDate:string) => {
-
- 
-            const id = mongoose.Types.ObjectId(salonid) // salon id
-            const date = moment() || moment(reqDate)
-            const salon = await Salon.findById(id)
-            
-            const starting_hours = salon.start_working_hours
-            var start_time = starting_hours.map(function (val) {
-                return moment(val).format('YYYY-MM-DD hh:mm a');;
-            })
-            console.log(start_time)
-            const end_hours = salon.end_working_hours
-            var end_time = end_hours.map(function (val) {
-                return moment(val).format('YYYY-MM-DD hh:mm a');;
-            })
-            const slots = []
-            var time1 = start_time[date.day()]
-            console.log(date)
-            var time2 = end_time[date.day()]
-            for (var m = moment(time1); m.isBefore(time2); m.add(30, 'minutes')) {
-                slots.push(m.format('hh:mm a'));
             }
-            return slots
+            for (let slot of employeeAbsentSlots) {
+                slot = moment(slot).format('hh:mm a')
+                if (slot === storeDate) {
+                    return {
+                        store_date: storeDate,
+                        absent: true
+                    }
+                }
+            }
+            return {
+                store_date: storeDate,
+                absent: false
+            }
+        })
+        return slots
 
     }
-    employeebyId = async (id:string) => {
-            const service = await Employee.findById(id).populate("services").populate("photo").exec()
-            return service
-    }
-    
+    slots = async (salonid: string, reqDate: string) => {
 
-    report = async (data:ReportVendorI)=>{
+
+        const id = mongoose.Types.ObjectId(salonid) // salon id
+        const date = moment() || moment(reqDate)
+        const salon = await Salon.findById(id)
+
+        const starting_hours = salon.start_working_hours
+        var start_time = starting_hours.map(function (val) {
+            return moment(val).format('YYYY-MM-DD hh:mm a');;
+        })
+        console.log(start_time)
+        const end_hours = salon.end_working_hours
+        var end_time = end_hours.map(function (val) {
+            return moment(val).format('YYYY-MM-DD hh:mm a');;
+        })
+        const slots = []
+        var time1 = start_time[date.day()]
+        console.log(date)
+        var time2 = end_time[date.day()]
+        for (var m = moment(time1); m.isBefore(time2); m.add(30, 'minutes')) {
+            slots.push(m.format('hh:mm a'));
+        }
+        return slots
+
+    }
+    employeebyId = async (id: string) => {
+        const service = await Employee.findById(id).populate("services").populate("photo").exec()
+        return service
+    }
+
+
+    report = async (data: ReportVendorI) => {
         console.log(data)
         const report = await this.reportVendorModel.create(data)
         return report
     }
-    feedback = async (data:FeedbackI)=>{
+    feedback = async (data: FeedbackI) => {
         console.log(data)
         const report = await this.feedbackVendorModel.create(data)
         return report
     }
-    
+
     vendorDelete = async (id: string) => {
-        const vendor = await this.model.findOneAndUpdate({_id:id},{blocked:true},{new:true})
+        const vendor = await this.model.findOneAndUpdate({ _id: id }, { blocked: true }, { new: true })
         return vendor
 
     }
 
-    vendorService = async(id:string,vendorId:string)=>{
-        const _id=mongoose.Types.ObjectId(id)
+    vendorService = async (id: string, vendorId: string) => {
+        const _id = mongoose.Types.ObjectId(id)
         const service = await Salon.aggregate()
-        .match({_id: _id})
-        .project({
-            name:1,
-            services: {$size:"$services"}
-        })
+            .match({ _id: _id })
+            .project({
+                name: 1,
+                services: { $size: "$services" }
+            })
         return service
     }
-    updateNotification = async (id:string,status:boolean)=>{
-        const notification = await this.model.findByIdAndUpdate(id,{notification:status},{new:true})
+    updateNotification = async (id: string, status: boolean) => {
+        const notification = await this.model.findByIdAndUpdate(id, { notification: status }, { new: true })
         return notification
     }
 
-    
+
 
 
 
