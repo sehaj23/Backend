7299cc372399655303f690c32043631fd6bca7e7
Preet Singh Chabra <sehaj23chawla@gmail.com>
2020-12-15 00:43:54 +0530

    time change

--- src/service/salon.service.ts
+++ src/service/salon.service.ts
@@ -831,11 +831,11 @@ export default class SalonService extends BaseService {
                         console.log(i.hours())
                         if(moment().day()-1 == day){
                         if(i.hours() > moment().hours()){
-                        const slot = moment(i.add(1,'hour')).local().format('hh:mm a')
+                        const slot = moment(i.add(1,'hour')).utcOffset("+05:30").format('hh:mm a')
                         
                         slots.push(slot)
                         }}else{
-                                const slot = moment(i).local().format('hh:mm a')
+                                const slot = moment(i).utcOffset("+05:30").format('hh:mm a')
                         
                                 slots.push(slot)   
                         
