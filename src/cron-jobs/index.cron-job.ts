a5b87d70f52243d2150a3a88170e3fed8282877c
codersingh27 <codersingh27@gmail.com>
2020-12-31 01:05:29 -0600

    cron commented

--- src/cron-jobs/index.cron-job.ts
+++ src/cron-jobs/index.cron-job.ts
@@ -1,18 +1,16 @@
 import moment = require("moment");
-import Cart from "../models/cart.model";
-import Salon from "../models/salon.model";
-import CartService from "../service/cart.service";
 
 var CronJob = require('cron').CronJob;
 // after one hour check if there is anything in the users cart
-var job = new CronJob('* * 1 * * *', function() {
-    const cartService = new CartService(Cart, Salon)
-    const todayDateMoment = moment(Date.now())
-    const todayDate = todayDateMoment.date
-    const carts = cartService.get({"createdAt": {
-            "$gt": todayDate
-        }
-    })
-    console.log(carts)
-}, null, true);
-job.start();
+// var job = new CronJob('* * 1 * * *', async function () {
+//     const cartService = new CartService(Cart, Salon)
+//     const todayDateMoment = moment(Date.now())
+//     const todayDate = todayDateMoment.date()
+//     const carts = await cartService.get({
+//         "createdAt": {
+//             "$gt": todayDate
+//         }
+//     })
+//     console.log(carts)
+// }, null, true);
+// job.start();
