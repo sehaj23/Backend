af59cba78f8dded493fad733f5531d5e61bd6004
codersingh27 <codersingh27@gmail.com>
2021-01-13 14:20:56 -0600

    code clean up

--- src/aws.ts
+++ src/aws.ts
@@ -1,13 +1,17 @@
-import * as AWS from "aws-sdk"
+import * as AWS from "aws-sdk";
 require('dotenv').config()
-
-AWS.config.getCredentials(function(err) {
-  if (err){} 
-  //console.log(err.stack);
-  // credentials not loaded
-  else {
-    AWS.config.update({region: 'ap-south-1'})
-    // console.log("Access key:", AWS.config.credentials.accessKeyId);
-    // console.log("Region: ", AWS.config.region);
+const activateAws = (activate: boolean = false) => {
+  if (activate) {
+    AWS.config.getCredentials(function (err) {
+      if (err) { }
+      //console.log(err.stack);
+      // credentials not loaded
+      else {
+        AWS.config.update({ region: 'ap-south-1' })
+        // console.log("Access key:", AWS.config.credentials.accessKeyId);
+        // console.log("Region: ", AWS.config.region);
+      }
+    });
   }
-});
+}
+export default activateAws
--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,17 +1,8 @@
 import * as admin from 'firebase-admin';
 
-// var serviceAccount = require("../../zattire-vendor-app-firebase-adminsdk-c56y9-2cef91fd53.json");
-
-// const firebase = admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://zattire-vendor-app.firebaseio.com"
-// });
-
-//var admin = require("firebase-admin");
-
 var serviceAccount = require("../../zattire-vendor-app-firebase-adminsdk-c56y9-2cef91fd53.json");
 
-const firebase=admin.initializeApp({
+const firebase = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://zattire-user-app.firebaseio.com"
 });
