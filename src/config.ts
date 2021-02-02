05d2cd94badcd1b363d254757bd76a69a5af3513
codersingh27 <codersingh27@gmail.com>
2021-02-02 00:04:02 -0600

    user jwt change 2 final

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ enum CONFIG {
     USER_JWT_KEY = "expelliarmus-8541#9%",
     RES_ERROR = "Server Error:",
     VENDOR_JWT = "thisisasecretkey",
-    USER_JWT = 'expectopatronum499.99^78',
+    USER_JWT = 'expectopatronum499saaxc99^7812skjhsgvcmkjhbvgqbwnjdhuqygvwb nju21y736tetdfgvbsnjwy76t7fdvbxhsjwytdfwgvqbhdr1t62yuhwjndhbgvdft617y8usxnjbdfghiuywtfscvbnk,mnbvcxsqasdfghjop098765rgvcxcvbhuytrewqasdfghjsdfghjaserfcvgydsasdfghjk123edfgyuiijhgfdsdfhsbhbdxasskjnssnakjiouygtf',
     EMP_JWT = "thisisemployeesecretkey",
     SALT = 'abrakadabra#6582@0216'
 }
