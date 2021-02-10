432bc32aad10e9a65c0248d35a380a6bf8f17a18
codersingh27 <codersingh27@gmail.com>
2021-02-10 10:40:43 -0600

    bug fix

--- src/routes/UserRoutes/refund.route.ts
+++ src/routes/UserRoutes/refund.route.ts
@@ -4,6 +4,7 @@ import UserverifyToken from "../../middleware/User.jwt";
 import Booking from "../../models/booking.model";
 import Cart from "../../models/cart.model";
 import MongoCounter from "../../models/mongo-counter.model";
+import Referral from "../../models/referral.model";
 import Refund from "../../models/refund.model";
 import Salon from "../../models/salon.model";
 import BookingService from "../../service/booking.service";
@@ -15,7 +16,7 @@ import { RefundValidator } from "../../validators/refund.validator";
 const refundRouter = Router()
 const cartService = new CartService(Cart, Salon)
 const mongoCounterService = new MongoCounterService(MongoCounter)
-const bookingService = new BookingService(Booking, Salon, cartService, mongoCounterService)
+const bookingService = new BookingService(Booking, Salon, cartService, mongoCounterService, Referral)
 const refundService = new RefundService(Refund, bookingService)
 const refundController = new RefundController(refundService)
 
