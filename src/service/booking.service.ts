dc1d811ac7aa8bb33f003a2edb7be103d6ef1529
Preet Singh Chabra <sehaj23chawla@gmail.com>
2021-02-16 21:43:25 +0530

    services time by default

--- src/service/booking.service.ts
+++ src/service/booking.service.ts
@@ -220,7 +220,7 @@ export default class BookingService extends BaseService {
      * 
      * @description This is the service to get the employees fof the salon on given date 
      */
-    getSalonEmployees = async (salonId: string, dateTime, employee: EmployeeSI[]) => {
+    getSalonEmployees =  async (salonId: string, dateTime, employee: EmployeeSI[]) => {
         const dateTimeAdd = moment(dateTime).add(15, 'minutes').format("YYYY-MM-DDTHH:mm:ss").concat(".000+00:00")
         const dateTimeSub = moment(dateTime).subtract(15, 'minutes').format("YYYY-MM-DDTHH:mm:ss").concat(".000+00:00")
         console.log("*****")
