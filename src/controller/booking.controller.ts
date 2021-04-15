c0d2bc8ceeeb083a66cb3bec1ca6d30500f84f47
Preet Singh Chabra <sehaj23chawla@gmail.com>
2021-04-15 14:30:33 +0530

    promocode service

--- src/controller/booking.controller.ts
+++ src/controller/booking.controller.ts
@@ -61,6 +61,7 @@ export default class BookingController extends BaseController {
         this.promoUserService = promoUserService
         this.referralService = referralService
         this.refundService = refundService
+        this.promoCodeService=promoCodeService
     }
 
 
