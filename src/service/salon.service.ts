72fa6caac2b248392bba41ba381bb90cd2149f24
Preet Singh Chabra <sehaj23chawla@gmail.com>
2021-06-02 15:59:13 +0530

    reviews

--- src/service/salon.service.ts
+++ src/service/salon.service.ts
@@ -607,11 +607,18 @@ export default class SalonService extends BaseService {
                 let pageLength: number = parseInt(q.page_length || 10)
                 pageLength = (pageLength > 100) ? 100 : pageLength
                 const skipCount = (pageNumber - 1) * pageLength
-                const reviewsAll = this.reviewModel.find({ salon_id: _id }).skip(skipCount).limit(pageLength).sort('-createdAt').populate({
-                        path: "user_id", populate: {
-                                path: 'profile_pic'
+                 const reviewsAll = this.reviewModel.find({ salon_id: _id }).skip(skipCount).limit(pageLength).sort('-createdAt').populate({
+                        path : 'user_id',
+                        select: { '_id': 1,'name':1},
+                        populate : {
+                          path : 'profile_pic'
                         }
-                })
+                      })
+                 //.populate({
+                //         path: "user_id",name:1,
+                        
+                        
+                // })
                 const reviewsPage = this.reviewModel.find({ salon_id: _id }).count();
 
                 const [reviews, pageNo] = await Promise.all([reviewsAll, reviewsPage])
