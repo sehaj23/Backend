b32b913ba6fb322968670ce35ea3ae99dfe6e6bf
codersingh27 <codersingh27@gmail.com>
2021-06-18 00:39:12 -0400

    admin wallet transactions

---
+++ src/routes/AdminRoutes/wallte-transaction.router.ts
@@ -0,0 +1,20 @@
+
+import { Router } from "express";
+import WalletTransactionController from "../../controller/wallet-transaction.controller";
+import UserverifyToken from "../../middleware/User.jwt";
+import Booking from "../../models/booking.model";
+import User from "../../models/user.model";
+import WalletTransaction from "../../models/wallet-transaction.model";
+import WalletRazorpay from "../../models/walletRazorpay.model";
+import UserService from "../../service/user.service";
+import WalletTransactionService from "../../service/wallet-transaction.service";
+import WalletRazorpayService from "../../service/walletRazorpay.service";
+
+const walletTransactionRouter = Router()
+const walletRazorpayService = new WalletRazorpayService(WalletRazorpay)
+const userService = new UserService(User, Booking)
+const walletTransactionService: WalletTransactionService = new WalletTransactionService(WalletTransaction, userService)
+const walletRazorpayController = new WalletTransactionController(walletTransactionService)
+
+walletTransactionRouter.get("/", UserverifyToken, walletRazorpayController.getWithPagination)
+export default walletTransactionRouter
