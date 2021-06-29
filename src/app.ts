46694b4108c17528af3cc9ac4286d6fad34d75b4
Preet Singh Chabra <sehaj23chawla@gmail.com>
2021-06-29 14:21:26 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -84,7 +84,7 @@ https.globalAgent.maxSockets = Infinity;
 app.use(compression())
 app.use(bodyParser({ limit: '50mb' }));
 app.use(cors({
-  origin: ['https://vendors.zattire.com', 'https://dev-vendor.zattire.com', 'http://localhost:3000', 'https://yumyam.zattire.com', 'https://prod-yamyum.zattire.com', 'https://dev2-vendor.zattire.com', "https://prodyum.zattire.com", "https://devyum.zattire.com", "http://localhost:59688"],
+  origin: ['https://vendors.zattire.com', 'https://dev-vendor.zattire.com', 'http://localhost:3000', 'https://yumyam.zattire.com', 'https://prod-yamyum.zattire.com', 'https://dev2-vendor.zattire.com', "https://prodyum.zattire.com", "https://devyum.zattire.com", "http://localhost:59688","https://zattire-vendor-app.web.app/#/"],
   credentials: true
 }));
 
