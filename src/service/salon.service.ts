3d75e1bb40698a1ee114d17709cc178327a9bcba
Preet Singh Chabra <sehaj23chawla@gmail.com>
2021-07-14 16:54:15 +0530

    checkfor houts

--- src/service/salon.service.ts
+++ src/service/salon.service.ts
@@ -1049,7 +1049,9 @@ export default class SalonService extends BaseService {
                 for (let i = selectedStartingHour; i.isBefore(selectedEndHour); i.add(30, 'minutes')) {
                         console.log(moment().format("DD/MM/YYYY"))
                         if (moment().format("DD/MM/YYYY") == moment(slotsDate).format("DD/MM/YYYY")) {
-                                if (moment(i).hours() > moment().utcOffset("-5:30").hours()) {
+                                if (i.utcOffset("5:30").hours()>moment().hours()) {
+                                        console.log(i.hour())
+                                        console.log(moment().hour())
                                         const slot = moment(i).add(30, 'minutes').format('hh:mm a')
 
                                         slots.push(slot)
