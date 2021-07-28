a743238baeb8aef99e442c284f13563b7b3d31a4
Preet Singh Chabra <sehaj23chawla@gmail.com>
2021-07-28 13:43:40 +0530

    user router

--- src/routes/AdminRoutes/user.router.ts
+++ src/routes/AdminRoutes/user.router.ts
@@ -30,6 +30,6 @@ userRouter.put("/:id", verifyToken, userController.put)
 userRouter.get("/search",userController.searchUsersByEmail)
 userRouter.put("/:id/photo", verifyToken, userController.putPhoto)
 userRouter.get("/:id/photo", verifyToken, userController.getPhoto)
-userRouter.post("/send-notification",userController.sendNotificationToUsers)
+userRouter.post("/send-notification",verifyToken,userController.sendNotificationToUsers)
 
 export default userRouter
