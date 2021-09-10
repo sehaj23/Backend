34ef785d9817f87cd09974e560856baf70302140
sehaj23 <sehaj23chawla@gmail.com>
2021-09-10 12:25:26 +0530

    promocode

--- src/routes/AdminAppRoutes/index.router.ts
+++ src/routes/AdminAppRoutes/index.router.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import adminRouter from "./admin.routes";
 import bookingRouter from "./booking.router";
 import loginRouter from "./login.router";
+import promoCodeRouter from "./promocode.router";
 import salonRouter from "./salon.router";
 
 const AdminApprouter = Router();
 AdminApprouter.use("/admin",adminRouter)
 AdminApprouter.use("/login", loginRouter);
 AdminApprouter.use("/booking", bookingRouter)
+AdminApprouter.use("/promocode",promoCodeRouter)
 AdminApprouter.use("/salon", salonRouter);
 
 
