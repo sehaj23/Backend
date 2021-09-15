dfb5db857143f32c831005c8502b3919216ee9bb
sehaj23 <sehaj23chawla@gmail.com>
2021-09-15 12:01:44 +0530

    Wallet router

--- src/routes/AdminAppRoutes/index.router.ts
+++ src/routes/AdminAppRoutes/index.router.ts
@@ -4,12 +4,14 @@ import bookingRouter from "./booking.router";
 import loginRouter from "./login.router";
 import promoCodeRouter from "./promocode.router";
 import salonRouter from "./salon.router";
+import walletTransactionRouter from "./wallet-trasanctionr.outer";
 
 const AdminApprouter = Router();
 AdminApprouter.use("/admin",adminRouter)
 AdminApprouter.use("/login", loginRouter);
 AdminApprouter.use("/booking", bookingRouter)
 AdminApprouter.use("/promocode",promoCodeRouter)
+AdminApprouter.use("/wallet",walletTransactionRouter)
 AdminApprouter.use("/salon", salonRouter);
 
 
