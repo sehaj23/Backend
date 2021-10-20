ba519257a49c567af137b5c5a4fcd2433b10b8fc
Sehaj <sehaj23chawla@gmail.com>
2021-10-21 01:23:35 +0530

    zattire services added

--- src/routes/AdminAppRoutes/index.router.ts
+++ src/routes/AdminAppRoutes/index.router.ts
@@ -7,6 +7,7 @@ import salonRouter from "./salon.router";
 import userRouter from "./user.router";
 
 import walletTransactionRouter from "./wallet-trasanctionr.outer";
+import zattireServiceRouter from "./zattire-service.router";
 
 const AdminApprouter = Router();
 AdminApprouter.use("/admin",adminRouter)
@@ -16,6 +17,7 @@ AdminApprouter.use("/promocode",promoCodeRouter)
 AdminApprouter.use("/wallet",walletTransactionRouter)
 AdminApprouter.use("/salon", salonRouter);
 AdminApprouter.use('/user', userRouter)
+AdminApprouter.use("/zattire-service",zattireServiceRouter)
 
 
 export default AdminApprouter
