3505854efd9540bee7c10d6646428584ac9e9cc7
Sehaj <sehaj23chawla@gmail.com>
2022-01-17 23:04:23 +0530

    city and location

--- src/controller/location.controller.ts
+++ src/controller/location.controller.ts
@@ -22,6 +22,9 @@ export default class LocationController extends BaseController {
         res.status(200).send({cities})
     })
     getSubAreaByCities = controllerErrorHandler( async (req: Request, res: Response) => {
+        if(!req.query.city){
+            return res.status(400).send({message:"send city in query"})
+        }
         const city = await this.locationService.get({city:req.query.city})
         let subarea = []
         city.map((e)=>{
