75f7a1819f91340e1aff5b1908ae47bdc1cce2a0
Sehaj <sehaj23chawla@gmail.com>
2022-01-29 14:32:53 +0530

    explore

--- src/models/explore.model.ts
+++ src/models/explore.model.ts
@@ -8,18 +8,25 @@ const exploreSchema = new mongoose.Schema({
         required: true,
         type: String,
     },
-    color: {
+    color: [{
         required: true,
         type: String,
-    },
+    }],
     photo: {
         type: String,
        required: true
     },
+    price:{
+        type:Number,
+        required:true
+    },
     salon_id: {
         type:  mongoose.Schema.Types.ObjectId,
          ref: "salons"  
-    }
+    },
+    tags:[{
+        type: String,
+    }]
 })
 const Explore = mongoose.model("explore", exploreSchema)
 
