3b4c90260032d3159f26d2b1ab097671ac09739d
mannnish <mk458978@gmail.com>
2022-09-19 19:36:51 +0530

    database and server ts restored

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
+console.log('Starting server....');
+
 import * as dotenv from "dotenv";
 import { httpApp } from "./app";
 import activateAws from "./aws";
 import * as db from "./database";
 import User from "./models/user.model";
 import firebase from "./utils/firebase";
+
 dotenv.config()
 
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
-const PORT = process.env.PORT || 8082;
 
-if (process.env.NODE_ENV !== 'test-api') {
+const PORT = process.env.PORT || 8082;
+if (process.env.NODE_ENV !== 'locals' && process.env.NODE_ENV !== 'test-api') {
     activateAws(true)
 }
-
 db.connectt().then(async () => {
 
     if (process.env.NODE_ENV === 'test-api') {
@@ -22,12 +24,12 @@ db.connectt().then(async () => {
         const user = await User.findOne({ _id: userId })
         if (user === null) throw new Error(`User not found with this id: ${userId}`)
     }
-    
-    // uncomment while merging to master
-    // tag: comment to work 
+
     if (cluster.isMaster && process.env.NODE_ENV !== 'local' && process.env.NODE_ENV !== 'test-api') {
         console.log(`Master ${process.pid} is running`);
 
+
+        // Fork workers.
         for (let i = 0; i < numCPUs; i++) {
             cluster.fork();
         }
@@ -47,14 +49,7 @@ db.connectt().then(async () => {
         console.log(`Worker ${process.pid} started`);
     }
 
-    //  ---------------------
-    // tag: uncomment to work
-    // const server = httpApp.listen(PORT, async () => {
-    //     const name = firebase.name
-    //     console.log(`Server is running http://localhost:${PORT}`);
-    //     console.log(`Firebase app name: ${name}`);
-    // });
-    //  ---------------------
+
 }).catch((e) => {
     console.error(`Db Error: ${e.message}`)
 })
